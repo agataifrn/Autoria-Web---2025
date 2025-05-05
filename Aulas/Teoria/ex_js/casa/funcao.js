06b1b98986b913b71b151e82d9736999f53b8e00
Ágata <freire.agata@escolar.ifrn.edu.br>
2025-05-05 20:17:09 -0300

    em casa

---
+++ Aulas/Teoria/ex_js/casa/funcao.js
@@ -0,0 +1,44 @@
+let listaProdutos = []
+
+function gravarProduto() {
+    let nomeProduto = document.getElementById("nomeProduto").value;
+    let qntdEstoque = document.getElementById("qntdEstoque").value;
+    let valorUnitario = document.getElementById("valorUnitario").value;
+    let fornecedor = document.getElementById("fornecedor").value;
+
+    let produto = {
+        nomeProduto : nomeProduto,
+        qntdEstoque : qntdEstoque,
+        valorUnitario : valorUnitario,
+        fornecedor : fornecedor
+    }
+
+    listaProdutos.push(produto);
+    listarProdutos();
+}
+
+function listarProdutos() {
+    let conteudo = '';
+    if (listaProdutos.length === 0) {
+        conteudo = `
+            <tr>
+                <td colspan="7">
+                    Nenhum produto cadastrado até o momento.
+                </td>
+            </tr>
+        `;
+    } else {
+        listaProdutos.forEach((produto, indice) => {
+            conteudo += `
+                <tr>
+                    <td>${indice}</td>
+                    <td>${produto.nomeProduto}</td>
+                    <td>${produto.qntdEstoque}</td>
+                    <td>${produto.valorUnitario}</td>
+                    <td>${produto.fornecedor}</td>
+                </tr>
+            `;
+        })
+    }
+    document.getElementById('conteudo').innerHTML = conteudo;
+}
